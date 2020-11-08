8d332ed873aa2cffd9f3d3ce2966f8d28836e551
Andres Monsalvo <69931247+andresmonsalvo@users.noreply.github.com>
2020-11-08 12:56:33 -0700

    ssl update

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    //ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
     //host: "postgresql-cylindrical-54295",
     //host: "127.0.0.1",
     //user: "postgres",
